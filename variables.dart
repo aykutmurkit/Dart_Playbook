f9c00381b046ea50f91555bdd5b98e0652e28210
Aykut Mürkit <44169438+aykutmurkit@users.noreply.github.com>
2022-10-05 06:46:04 +0300

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,20 @@
+void main() {
+
+  
+	print("wake up neo");
+
+  int x = 1;
+  double pi = 3.14;
+  String name = "aykut";
+  bool loginCheck = true;
+
+  //variable uses camelcase
+  //variable names can not be start with number
+
+
+  // we can not use string with number. We have to change the format before because it's trying to do addition.
+  
+  print("pi number is "+pi.toString());
+
+  
+}
